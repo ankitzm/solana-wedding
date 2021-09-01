8b263e1b3d19b8e9ab9924ffe934ea7c316f005f
ankitzm <ankit.singh.zm@gmail.com>
2021-09-01 19:58:50 +0530

    home button added

--- src/pages/certificatePage/index.js
+++ src/pages/certificatePage/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
 import Certificate from '../../component/certificate/Certificate'
 import WalletAdapter from '../../component/wallet/WalletAdapter'
+import "./style.css"
 
 const CertificatePage = () => {
     const [Data, setData] = useState(null)
@@ -12,6 +14,9 @@ const CertificatePage = () => {
     console.log(Data); // checking
     return (
         <div>
+            <Link class="home-btn" to="/">
+            <img src="https://img.icons8.com/glyph-neue/38/262626/home.png" alt="home" />
+            </Link>
             <WalletAdapter Data={Data} />
             <Certificate sendDataToParent={sendDataToParent} />
         </div>
