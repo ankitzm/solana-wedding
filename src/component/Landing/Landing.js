537f5bc90ea14d7c9907e34520a82fb62290d285
ankitzm <ankit.singh.zm@gmail.com>
2021-09-01 14:03:25 +0530

    animation added

--- src/component/Landing/Landing.js
+++ src/component/Landing/Landing.js
@@ -3,15 +3,15 @@ import { LeftLeaf, RightLeaf, SolanaLogo } from "../../assets/index"
 
 function Landing() {
     return (
-            <div className="home">
-                <img src={LeftLeaf} alt="left-leaf" className="leaf" />
-                <div className="heading">
-                    <img className="solana-logo" src={SolanaLogo} alt="" /> <br />
-                    WEDDING
-                    <p className="sub-heading">we connects</p>
-                </div>
-                <img src={RightLeaf} alt="right-leaf" className="leaf" />
+        <div className="home">
+            <img src={LeftLeaf} alt="left-leaf" className="leaf" data-aos="fade-left" />
+            <div className="heading" data-aos="zoom-in-up">
+                <img className="solana-logo" src={SolanaLogo} alt="" /> <br />
+                WEDDING
+                <p className="sub-heading">we connects</p>
             </div>
+            <img src={RightLeaf} alt="right-leaf" className="leaf" data-aos="fade-right"/>
+        </div>
     )
 }
 
