55b96853ebdfaa5f7432563a68f5fd5247b01aa0
ankitzm <ankit.singh.zm@gmail.com>
2021-09-01 12:18:37 +0530

    api fixed fo rexplorer

--- src/component/explorer/Explorer.js
+++ src/component/explorer/Explorer.js
@@ -4,15 +4,17 @@ import Get from './callAPI'
 import CoreBTN from '../../core/btn/btn'
 
 function Explorer() {
-    const [title, setTitle] = useState('')
-
+    const [search, setSearch] = useState('')
+    const [mainJSON, setMainJSON] = useState(null)
+    console.log(mainJSON);
+    
     return (
         <div className="explorer">
-            <input type="text" onChange={event => setTitle(event.target.value)} placeholder="Transaction ID" />
+            <input type="text" onChange={e => e.target.value !== '' ? setSearch(e.target.value) : "disabled"} placeholder="Transaction ID" />
             <CoreBTN
                 id="toggle"
                 onClick={e => (
-                    Get(title)
+                    search !== '' ? setMainJSON(Get(search)) : "disabled"
                 )}
                 text="Get Data"
                 type="dark"
@@ -24,14 +26,6 @@ function Explorer() {
 
             {/* show recieved data */}
             <div id="recieved-data" className="recieved">
-
-                <div className="formatted">
-                    <strong>The Transaction ID has been verified !!</strong> <br /><br />
-                    Husband Name &nbsp;:  <p id="husband"></p><br />
-                    Wife Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:  <p id="wife"></p><br />
-                    Wedding Date &nbsp;&nbsp;&nbsp;:  <p id="date"></p> <br />
-                </div>
-
                 <div className="cert-pdf">
                     <embed src="http://ankitzm.co/template/template3.jpg" type="application/pdf" frameBorder="0"
                         scrolling="none" height="100%" width="100%"></embed>
--- src/component/explorer/callAPI/index.js
+++ src/component/explorer/callAPI/index.js
@@ -1,22 +1,14 @@
-// headers were common in both api calls
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+function Get(search) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-function Get(form) {
-
-    // change text when button is clicked
-    document.getElementById("temp").innerHTML = "Searching ...";
-
-    // get signature from address
     var raw = JSON.stringify({
         "jsonrpc": "2.0",
         "id": 1,
-        "method": "getSignaturesForAddress",
+        "method": "getTransaction",
         "params": [
-            form,
-            {
-                "limit": 1
-            }
+            search,
+            "json"
         ]
     });
 
@@ -29,51 +21,18 @@ function Get(form) {
 
     fetch("https://api.devnet.solana.com", requestOptions)
         .then(response => response.json())
-        .then(result => {
-            var signature = result.result[0].signature;
-
-            // console.log(signature);                               check
-            GetData(signature)
-        })
-        .catch(error => console.log('error', error));
-
-}
-
-// signature -->  extract important data (husband name, wife name, data)
-function GetData(signature) {
-    var raw = JSON.stringify({
-        jsonrpc: "2.0",
-        id: 1,
-        method: "getTransaction",
-        params: [signature, "json"],
-    });
-
-    var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow",
-    };
-
-    fetch("https://api.devnet.solana.com/", requestOptions)
-        .then((response) => response.json())
         .then(data => {
             var main = data.result.meta.logMessages[4];
 
             // remove unnessasary text and trimmed important data to JSON
             var mainJSON = JSON.parse(main.substring(main.indexOf("{"), main.indexOf("}") + 1));
-            // console.log(mainJSON);                       check
+            // console.log("from index" + mainJSON);                      // check
 
-
-            document.getElementById("husband").innerText = mainJSON.husband;
-            document.getElementById("wife").innerText = mainJSON.wife;
-            document.getElementById("date").innerText = mainJSON.date;
-
-            // removing text in div having id "temp"
-            document.getElementById("temp").style.display = "none";
-
+            return mainJSON;
         })
-        .catch((error) => console.log("error", error));
+        .catch(error => console.log('error', error));
+
 }
 
-export default Get
+export default Get;
+
