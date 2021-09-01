4e0de0eeffad60dd4e74a5a005d6cf02bd84241b
BhautikPoriya <bporiya2001@gmail.com>
2021-09-01 11:47:19 +0530

    button design
    changes button design

--- src/core/btn/btn.js
+++ src/core/btn/btn.js
@@ -2,9 +2,17 @@ import "./btn.css"
 
 function CoreBTN({ type, text, onClick, id }) {
     return (<>
-        <button id={id} onClick={onClick} className={`btn ${type}`}>
+        {/* <button id={id} onClick={onClick} className={`btn ${type}`}>
             {text}
-        </button></>
+        </button> */ 
+        <a id={id} onClick={onClick} className={`btn ${type}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            {text}
+        </a>
+        }</>
     )
 }
 
