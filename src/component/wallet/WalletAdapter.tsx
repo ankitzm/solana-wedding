69ac6db93013d0461c5de7b284d015cb0ebfe110
avish mehta <avish@logicwind.co>
2021-09-01 23:23:49 +0400

    api

--- src/component/wallet/WalletAdapter.tsx
+++ src/component/wallet/WalletAdapter.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import React, { useEffect, useMemo, useState } from "react"
-import "./WalletAdapter.css"
-import Wallet from "../../dist/cjs/index"
+import React, { useEffect, useMemo, useState } from 'react';
+import './WalletAdapter.css';
+import Wallet from '../../dist/cjs/index';
 import {
 	Connection,
 	SystemProgram,
@@ -12,23 +12,23 @@ import {
 	Keypair,
 	TransactionInstruction,
 	// sendAndConfirmTransaction,
-} from "@solana/web3.js"
-import * as borsh from "borsh"
-import CoreBTN from "../../core/btn/btn"
+} from '@solana/web3.js';
+import * as borsh from 'borsh';
+import CoreBTN from '../../core/btn/btn';
 
 function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
-	const [logs, setLogs] = useState<string[]>([])
+	const [logs, setLogs] = useState<string[]>([]);
 	function addLog(log: string) {
-		setLogs(logs => [...logs, log])
+		setLogs((logs) => [...logs, log]);
 	}
 
-	const network = clusterApiUrl("devnet")
-	const [providerUrl, setProviderUrl] = useState("https://www.sollet.io")
-	const connection = useMemo(() => new Connection(network), [network])
+	const network = clusterApiUrl('devnet');
+	const [providerUrl, setProviderUrl] = useState('https://www.sollet.io');
+	const connection = useMemo(() => new Connection(network), [network]);
 	const urlWallet = useMemo(
 		() => new Wallet(providerUrl, network),
-		[providerUrl, network],
-	)
+		[providerUrl, network]
+	);
 	// const injectedWallet = useMemo(() => {
 	//   try {
 	//     return new Wallet(
@@ -42,68 +42,80 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 	// }, [network]);
 	const [selectedWallet, setSelectedWallet] = useState<
 		Wallet | undefined | null
-	>(undefined)
-	const [, setConnected] = useState(false)
+	>(undefined);
+	const [, setConnected] = useState(false);
 	useEffect(() => {
 		if (selectedWallet) {
-			selectedWallet.on("connect", () => {
-				setConnected(true)
+			selectedWallet.on('connect', () => {
+				setConnected(true);
 				addLog(
-					`Connected to wallet ${selectedWallet.publicKey?.toBase58() ?? "--"
-					}`,
-				)
-			})
-			selectedWallet.on("disconnect", () => {
-				setConnected(false)
-				addLog("Disconnected from wallet")
-			})
-			void selectedWallet.connect()
+					`Connected to wallet ${selectedWallet.publicKey?.toBase58() ?? '--'}`
+				);
+			});
+			selectedWallet.on('disconnect', () => {
+				setConnected(false);
+				addLog('Disconnected from wallet');
+			});
+			void selectedWallet.connect();
 			return () => {
-				void selectedWallet.disconnect()
-			}
+				void selectedWallet.disconnect();
+			};
 		}
-	}, [selectedWallet])
+	}, [selectedWallet]);
 
 	class GreetingAccount {
-		txt = ""
+		txt = '';
 		constructor(fields: { txt: string } | undefined = undefined) {
 			if (fields) {
-				this.txt = fields.txt
+				this.txt = fields.txt;
 			}
 		}
 	}
-	const completeData = JSON.stringify(Data)
-	const transactionLength = completeData.length
-	console.log(completeData.length)
+	const completeData = JSON.stringify(Data);
+	const transactionLength = completeData.length;
+	console.log(completeData.length);
 
 	const GreetingSchema = new Map([
-		[GreetingAccount, { kind: "struct", fields: [["txt", "String"]] }],
-	])
-	const sampleGreeter = new GreetingAccount()
-	sampleGreeter.txt = "0".repeat(transactionLength)
-	const GREETING_SIZE = borsh.serialize(GreetingSchema, sampleGreeter).length
+		[GreetingAccount, { kind: 'struct', fields: [['txt', 'String']] }],
+	]);
+	const sampleGreeter = new GreetingAccount();
+	sampleGreeter.txt = '0'.repeat(transactionLength);
+	const GREETING_SIZE = borsh.serialize(GreetingSchema, sampleGreeter).length;
 
-	let greetedPubkey: PublicKey
+	let greetedPubkey: PublicKey;
 
 	const programID = new PublicKey(
-		"81MbSQG6DaoCuvqgCCzWYXP5AWUanxyFvsTodSH8vh9A",
-	)
+		'81MbSQG6DaoCuvqgCCzWYXP5AWUanxyFvsTodSH8vh9A'
+	);
 	// const GREETING_SEED = "hello";
 
 	async function sendTransaction() {
 		try {
-			const pubkey = selectedWallet?.publicKey
+			const pubkey = selectedWallet?.publicKey;
 			if (!pubkey || !selectedWallet) {
-				throw new Error("wallet not connected")
+				throw new Error('wallet not connected');
 			}
 			// console.log(pubkey);
-			const newAccount = new Keypair()
-			greetedPubkey = newAccount.publicKey
-			console.log(greetedPubkey.toBase58())
+			const newAccount = new Keypair();
+			greetedPubkey = newAccount.publicKey;
+			console.log(greetedPubkey.toBase58());
 
 			const lamports = await connection.getMinimumBalanceForRentExemption(
-				GREETING_SIZE,
-			)
+				GREETING_SIZE
+			);
+			const checkbalance = await connection.getBalance(greetedPubkey);
+			if (checkbalance < lamports) {
+				const data = await fetch(
+					'https://meme-api.herokuapp.com/gimme/wholesomememes',
+					{
+						method: 'GET',
+					}
+				)
+					.then((res) => res.json())
+					.then((res) => <img src={res.preview[5]} alt="image"></img>);
+			} else {
+				throw new Error('Not enough lamports');
+			}
 
 			const instruction = new TransactionInstruction(
 				SystemProgram.createAccount({
@@ -112,24 +124,24 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 					lamports: lamports,
 					space: GREETING_SIZE,
 					programId: programID,
-				}),
-			)
+				})
+			);
 
-			const transaction = new Transaction().add(instruction)
+			const transaction = new Transaction().add(instruction);
 
 			////////////////////////////////
 
 			/////////////////////////////////
-			const husband = Data.Husband
-			const wife = Data.Wife
-			const date = Data.Date
+			const husband = Data.Husband;
+			const wife = Data.Wife;
+			const date = Data.Date;
 
 			const allData = {
 				husband: husband,
 				wife: wife,
 				date: date,
-			}
-			let msg = JSON.stringify(allData)
+			};
+			let msg = JSON.stringify(allData);
 			// if (msg.length > 200) {
 			//   throw new Error("Message must be less than 30 characters");
 			// }
@@ -137,8 +149,8 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 			//   msg = msg + "*".repeat(200 - msg.length);
 			// }
 
-			const messageAccount = new GreetingAccount()
-			messageAccount.txt = msg
+			const messageAccount = new GreetingAccount();
+			messageAccount.txt = msg;
 			const sending_Data = new TransactionInstruction({
 				keys: [
 					{
@@ -148,31 +160,27 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 					},
 				],
 				programId: programID,
-				data: Buffer.from(
-					borsh.serialize(GreetingSchema, messageAccount),
-				), // All instructions are hellos
-			})
-			transaction.add(sending_Data)
+				data: Buffer.from(borsh.serialize(GreetingSchema, messageAccount)), // All instructions are hellos
+			});
+			transaction.add(sending_Data);
 
 			////////////////////////////////
 			// addLog("Getting recent blockhash");
 			transaction.recentBlockhash = (
 				await connection.getRecentBlockhash()
-			).blockhash
+			).blockhash;
 			// addLog("Sending signature request to wallet");
-			transaction.feePayer = pubkey
-			const signed = await selectedWallet.signTransaction(transaction)
-			transaction.partialSign(newAccount)
+			transaction.feePayer = pubkey;
+			const signed = await selectedWallet.signTransaction(transaction);
+			transaction.partialSign(newAccount);
 			// addLog("Got signature, submitting transaction");
-			const signature = await connection.sendRawTransaction(
-				signed.serialize(),
-			)
+			const signature = await connection.sendRawTransaction(signed.serialize());
 			// addLog("Submitted transaction " + signature + ", awaiting confirmation");
-			await connection.confirmTransaction(signature, "singleGossip")
-			addLog("Transaction " + signature + " confirmed")
-			const balance = await connection.getBalance(greetedPubkey)
+			await connection.confirmTransaction(signature, 'singleGossip');
+			addLog('Transaction ' + signature + ' confirmed');
+			const balance = await connection.getBalance(greetedPubkey);
 			// eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-			addLog("Balance currently: " + balance)
+			addLog('Balance currently: ' + balance);
 			////////////////////////////////
 			// const transaction2 = new Transaction().add(sending_Data);
 			// addLog('Getting recent blockhash');
@@ -196,10 +204,19 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 			// // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
 			// addLog('Balance currently: ' + balance2);
 
-			console.log("Done!!")
+			console.log('Done!!');
+			// fetch('https://meme-api.herokuapp.com/gimme/wholesomememes', {
+			// 	method: 'GET',
+			// })
+			// 	.then((res) => {
+			// 		res.json();
+			// 	})
+			// 	.then((data) => {
+			// 		console.log(data);
+			// 	});
 		} catch (e) {
-			console.warn(e)
-			alert(`Error: ${(e as Error).message}`)
+			console.warn(e);
+			alert(`Error: ${(e as Error).message}`);
 		}
 	}
 
@@ -209,8 +226,7 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 			{/* <div>Network: {network}</div> */}
 			<div>
 				Select Wallet : <br />
-
-				<select onChange={e => setProviderUrl(e.target.value)}>
+				<select onChange={(e) => setProviderUrl(e.target.value)}>
 					<option value="https://solflare.com/">Sollet</option>
 					<option value="https://phantom.app/">Phantom</option>
 					<option value="https://solflare.com/">Solflare</option>
@@ -218,9 +234,7 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 			</div>
 			{selectedWallet && selectedWallet.connected ? (
 				<div>
-					<div>
-						Wallet address: {selectedWallet.publicKey?.toBase58()}.
-					</div>
+					<div>Wallet address: {selectedWallet.publicKey?.toBase58()}.</div>
 					<CoreBTN
 						onClick={sendTransaction}
 						text="Send Transaction"
@@ -255,7 +269,7 @@ function WalletAdapter({ Data }: { Data: any }): React.ReactElement {
 				))}
 			</div>
 		</div>
-	)
+	);
 }
 
-export default WalletAdapter
+export default WalletAdapter;
