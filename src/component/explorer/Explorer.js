b0ad4891ef81cd6136d261cacd4e148abb770d06
ankitzm <ankit.singh.zm@gmail.com>
2021-09-01 14:02:21 +0530

    some logic added

--- src/component/explorer/Explorer.js
+++ src/component/explorer/Explorer.js
@@ -6,32 +6,34 @@ import CoreBTN from '../../core/btn/btn'
 function Explorer() {
     const [search, setSearch] = useState('')
     const [mainJSON, setMainJSON] = useState(null)
-    
+
     return (
         <div className="explorer">
             <input type="text" onChange={e => e.target.value !== '' ? setSearch(e.target.value) : "disabled"} placeholder="Transaction ID" />
             <CoreBTN
                 id="toggle"
                 onClick={e => (
-                    search !== '' ? setMainJSON(Get(search)) : "disabled"
+                    setMainJSON(Get(search))
                 )}
                 text="Get Data"
                 type="dark"
             />
 
-            <div id="temp">
-                Search your transaction ID above
-            </div>
-
-            {/* show recieved data */}
-            <div id="recieved-data" className="recieved">
-                <div className="cert-pdf">
-                    {mainJSON}
-                    <embed src="http://ankitzm.co/template/template3.jpg" type="application/pdf" frameBorder="0"
-                        scrolling="none" height="100%" width="100%"></embed>
+            {/* terenary operator */}
+            {mainJSON === null ?
+                <div id="temp">
+                    Search your transaction ID above
                 </div>
-            </div>
-        </div>
+                :
+                <div id="recieved-data" className="recieved">
+                    <div className="cert-pdf">
+                        {mainJSON}
+                        <embed src="http://ankitzm.co/template/template3.jpg" type="application/pdf" frameBorder="0"
+                            scrolling="none" height="100%" width="100%"></embed>
+                    </div>
+                </div>
+            }
+        </div >
     )
 }
 
