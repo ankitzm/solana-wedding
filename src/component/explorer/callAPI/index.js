30f55b476a40ba986275167ca7cf3fedab76de79
ankitzm <ankit.singh.zm@gmail.com>
2021-09-03 20:07:41 +0530

    speed up the code

--- src/component/explorer/callAPI/index.js
+++ src/component/explorer/callAPI/index.js
@@ -3,7 +3,7 @@ import moment from "moment";
 function Get(search) {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-
+    
     var raw = JSON.stringify({
         "jsonrpc": "2.0",
         "id": 1,
@@ -13,36 +13,35 @@ function Get(search) {
             "json"
         ]
     });
-
+    
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
-
+    // add QR code
+    var QRimg = document.getElementById("QR")
+    QRimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${search}`;
+    QRimg.style.width = "80px"
+    QRimg.style.height = "80px"
+    document.getElementById = QRimg
     fetch("https://api.devnet.solana.com", requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            var main = data.result.meta.logMessages[4];
-
-            // remove unnessasary text and trimmed important data to JSON
-            var mainJSON = JSON.parse(main.substring(main.indexOf("{"), main.indexOf("}") + 1));
-            // console.log("from index" + mainJSON);                      // check
-
-            document.getElementById("husband").innerHTML = mainJSON.husband
-            document.getElementById("wife").innerHTML = mainJSON.wife
-            document.getElementById("date").innerHTML = moment(mainJSON.date).format("DD MMMM YYYY")
-
-            // add QR code
-            var QRimg = document.getElementById("QR")
-            QRimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${search}`;
-            QRimg.style.width = "80px"
-            QRimg.style.height = "80px"
-            document.getElementById = QRimg
-            
-        })
-        .catch(error => console.log('error', error));
+    .then(response => response.json())
+    .then(data => {
+        var main = data.result.meta.logMessages[4];
+        
+        // remove unnessasary text and trimmed important data to JSON
+        var mainJSON = JSON.parse(main.substring(main.indexOf("{"), main.indexOf("}") + 1));
+        // console.log("from index" + mainJSON);                      // check
+        
+        document.getElementById("husband").innerHTML = mainJSON.husband
+        document.getElementById("wife").innerHTML = mainJSON.wife
+        document.getElementById("date").innerHTML = moment(mainJSON.date).format("DD MMMM YYYY")
+        
+        
+    })
+    .catch(error => console.log('error', error));
 }
 
 export default Get;
