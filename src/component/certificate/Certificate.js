b0958adbed3c4d0f2c8231b1b7b4255989d1d6e3
Harsh Ghodkar <harshghodkar@gmail.com>
2021-09-04 22:16:14 +0530

    commented download button

--- src/component/certificate/Certificate.js
+++ src/component/certificate/Certificate.js
@@ -6,76 +6,76 @@ import CertImg from "./../../assets/cert.jpg";
 import moment from "moment";
 
 const Certificate = ({ sendDataToParent }) => {
-    const certificateWrapper = React.createRef();
+  const certificateWrapper = React.createRef();
 
-    const [Husband, setHusband] = useState("");
-    const [Wife, setWife] = useState("");
-    const [Date, setDate] = useState("");
+  const [Husband, setHusband] = useState("");
+  const [Wife, setWife] = useState("");
+  const [Date, setDate] = useState("");
 
-    const Data = {
-        Husband: Husband,
-        Wife: Wife,
-        Date: Date,
-    };
-    let txid;
+  const Data = {
+    Husband: Husband,
+    Wife: Wife,
+    Date: Date,
+  };
+  let txid;
 
-    //   Data.Date = moment(Data.Date).format("DD MMMM YYYY");
+  //   Data.Date = moment(Data.Date).format("DD MMMM YYYY");
 
-    return (
-        <div>
-            <div className="certificate-container">
-                <div className="form">
-                    <form id="form">
-                        <input
-                            type="text"
-                            placeholder="Husband's Name"
-                            value={Husband}
-                            onChange={(e) => setHusband(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Wife's Name"
-                            value={Wife}
-                            onChange={(e) => setWife(e.target.value)}
-                        />
-                        <input
-                            type="date"
-                            value={Date}
-                            onChange={(e) =>
-                                setDate(e.target.value)
-                            }
-                        />
-                        {/* {txid} */}
-                        <CoreBTN
-                            onClick={(e) => {
-                                e.preventDefault();
-                                sendDataToParent(Data);
-                            }}
-                            text="Confirm Data"
-                            type="light"
-                        />
-                        <span className="sub-text">
-                            {" "}
-                            Click Send Transaction to complete your Transaction{" "}
-                        </span>
-                    </form>
-                </div>
-
-                <div className="download">
-                    <div id="downloadWrapper" ref={certificateWrapper}>
-                        <div id="certificateWrapper">
-                            <p id="husband">{Husband}</p>
-                            <p id="wife">{Wife}</p>
-                            <script>txid = document.cookie;</script>
-                            <p id="date">{Date === "" ? "" : moment(Date).format("DD MMMM YYYY")}</p>
-
-                            {/* image is in ration 4:3 */}
-                            <img id="cert-new" src={CertImg} alt="Certificate" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="certificate-container">
+        <div className="form">
+          <form id="form">
+            <input
+              type="text"
+              placeholder="Husband's Name"
+              value={Husband}
+              onChange={(e) => setHusband(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Wife's Name"
+              value={Wife}
+              onChange={(e) => setWife(e.target.value)}
+            />
+            <input
+              type="date"
+              value={Date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            {/* {txid} */}
             <CoreBTN
+              onClick={(e) => {
+                e.preventDefault();
+                sendDataToParent(Data);
+              }}
+              text="Confirm Data"
+              type="light"
+            />
+            <span className="sub-text">
+              {" "}
+              Click Send Transaction to complete your Transaction{" "}
+            </span>
+          </form>
+        </div>
+
+        <div className="download">
+          <div id="downloadWrapper" ref={certificateWrapper}>
+            <div id="certificateWrapper">
+              <p id="husband">{Husband}</p>
+              <p id="wife">{Wife}</p>
+              <script>txid = document.cookie;</script>
+              <p id="date">
+                {Date === "" ? "" : moment(Date).format("DD MMMM YYYY")}
+              </p>
+
+              {/* image is in ration 4:3 */}
+              <img id="cert-new" src={CertImg} alt="Certificate" />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <CoreBTN
                 onClick={(e) => {
                     if (document.cookie != null) {
                         txid = document.cookie;
@@ -88,9 +88,9 @@ const Certificate = ({ sendDataToParent }) => {
                 }}
                 type="light"
                 text="download"
-            />
-        </div>
-    );
+            /> */}
+    </div>
+  );
 };
 
 export default Certificate;
