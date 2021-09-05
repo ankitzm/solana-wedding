a5e3a905a7ff5b6a5e0d5e66740d1365a3617689
Harsh Ghodkar <harshghodkar@gmail.com>
2021-09-05 18:58:33 +0530

    Flip wala

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import ProfileCard from './card/ProfileCard';
-import './Profile.css';
+import React from "react";
+import ProfileCard from "./card/ProfileCard";
+import "./Profile.css";
 
 function Profile() {
-	return (
-		<div className="profile container">
-			<div className="text"> Our Beloved Team </div>
-			<div className="profiles">
-				<ProfileCard
-					Picture="https://media-exp1.licdn.com/dms/image/C4E03AQE65SLDzmO-zQ/profile-displayphoto-shrink_400_400/0/1628513980585?e=1635984000&v=beta&t=4nbVo87mPKCtgdWBvIODpxvrCjpcMO3YRC0uxoTFUx4"
-					Name="Ankit Singh"
-					About="Frontend Developer"
-				/>
-				<ProfileCard
-					Picture="https://media-exp1.licdn.com/dms/image/C4D03AQEH8quRVh57bg/profile-displayphoto-shrink_200_200/0/1623261924566?e=1635984000&v=beta&t=pZ5Gkt_SzSYbggQyez5hA2tjUbC0_pIs7dWpQkppA3A"
-					Name="Avish Mehta"
-					About="Backend Developer"
-				/>
-				<ProfileCard
-					Picture="https://avatars.githubusercontent.com/u/71444879?v=4"
-					Name="Bhautik Poriya"
-					About="UI/UX Designer"
-				/>
-				<ProfileCard
-					Picture="https://avatars.githubusercontent.com/u/71517788?v=4"
-					Name="Harsh Ghodkar"
-					About="Blockchain Developer"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="profile container">
+      <div className="text"> Our Beloved Team </div>
+      <div className="profiles">
+        <ProfileCard
+          Picture="https://media-exp1.licdn.com/dms/image/C4E03AQE65SLDzmO-zQ/profile-displayphoto-shrink_400_400/0/1628513980585?e=1635984000&v=beta&t=4nbVo87mPKCtgdWBvIODpxvrCjpcMO3YRC0uxoTFUx4"
+          Name="Ankit Singh"
+          About="Frontend Developer"
+          Back_Side="https://www.linkedin.com/in/ankitzm/"
+        />
+        <ProfileCard
+          Picture="https://media-exp1.licdn.com/dms/image/C4D03AQEH8quRVh57bg/profile-displayphoto-shrink_200_200/0/1623261924566?e=1635984000&v=beta&t=pZ5Gkt_SzSYbggQyez5hA2tjUbC0_pIs7dWpQkppA3A"
+          Name="Avish Mehta"
+          About="Backend Developer"
+        />
+        <ProfileCard
+          Picture="https://avatars.githubusercontent.com/u/71444879?v=4"
+          Name="Bhautik Poriya"
+          About="UI/UX Designer"
+        />
+        <ProfileCard
+          Picture="https://avatars.githubusercontent.com/u/71517788?v=4"
+          Name="Harsh Ghodkar"
+          About="Blockchain Developer"
+        />
+      </div>
+    </div>
+  );
 }
-
 
 export default Profile;
--- src/component/Profile/card/ProfileCard.js
+++ src/component/Profile/card/ProfileCard.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./ProfileCard.css";
 
-const ProfileCard = ({ Picture, Name, About }) => {
-    return (
-        <div className="profile-card">
-
-            <div className="card-container">
-                <img className="round" src={Picture} height="200px" alt="user" />
-                <h3>{Name}</h3>
-                <h6>India</h6>
-                <h4>{About}</h4>
-
-            </div>
-
-
+const ProfileCard = ({ Picture, Name, About, Back_Side }) => {
+  return (
+    <div className="profile-card">
+      <div className="card-container">
+        <div class="card">
+          <div class="card-side front">
+            <img className="round" src={Picture} height="200px" alt="user" />
+            <h3>{Name}</h3>
+            <h6>India</h6>
+            <h4>{About}</h4>
+          </div>
+          <div class="card-side back">
+            <div>{Back_Side}</div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default ProfileCard;
